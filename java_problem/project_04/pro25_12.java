2d66a3535ba9ae1c8844b1ce76c02c8aed873341
민기의 허브 <163251752+jajaham4@users.noreply.github.com>
2024-04-25 19:33:28 +0900

    Add files via upload

---
+++ java_problem/project_04/pro25_12.java
@@ -0,0 +1,25 @@
+/*
+ 문제번호 - 10171번
+ -문제
+  아래 예제와 같이 고양이를 출력하시오.
+   \    /\
+    )  ( ')
+   (  /  )
+    \(__)|
+ -입력
+  X
+ -출력
+  고양이를 출력한다.
+*/
+package project_04;
+
+public class pro25_12 {
+
+	public static void main(String[] args) {	
+		System.out.println("\\    /\\");
+	    System.out.println(" )  ( ')");
+	    System.out.println("(  /  )");
+	    System.out.println(" \\(__)|");				
+	}
+
+}
