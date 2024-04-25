318bc64f93dac696909cdc43433fbe7fa3b14553
민기의 허브 <163251752+jajaham4@users.noreply.github.com>
2024-04-25 18:55:44 +0900

    Update pro25_06.java

--- java_problem/project_04/pro25_06.java
+++ java_problem/project_04/pro25_06.java
@@ -1,23 +1,27 @@
 /*
- 문제번호 - 1008번
+ 문제번호 - 10869번
  -문제
- 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
  -입력
- 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
  -출력
- 첫째 줄에 A/B를 출력한다
+ 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
 */
 package project_04;
 
 import java.util.Scanner;
 
-public class pro25_06 {
+public class pro25_05 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
 		System.out.println(a/b);
+		System.out.println(a%b);
 	}
 
-}
+}	
