7e1f19fc3b2fbff3f4608b07d599975ab5d92f5b
민기의 허브 <163251752+jajaham4@users.noreply.github.com>
2024-04-25 18:32:33 +0900

    Add files via upload

---
+++ java_problem/project_04/pro25_01.java
@@ -0,0 +1,16 @@
+/*
+ 첫번째 문제
+ -문제
+ Hello World!를 출력하시오.
+ -입력
+ x
+ -출력
+ Hello World!를 출력하시오.
+ */
+package project_04;
+
+public class pro25_01 {
+	public static void main(String[] args) {
+		System.out.println("hello World");
+	}
+}
